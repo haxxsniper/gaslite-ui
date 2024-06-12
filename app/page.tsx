626793ee5f470d78109a5716703251ee55ee6752
ZxStim <thang.victor.luong@gmail.com>
2024-06-13 00:05:45 +0700

    edit erc20 card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import AirdropErc20Card from "@/components/airdrop-erc20-card";
 import AirdropKaiaCard from "@/components/airdrop-kaia-card";
 
 export default function Home() {
   return (
-    <div className="text-left w-[768px]">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Airdrop</h1>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">A simple tool to airdrop on Kaia chain</p>
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-16 text-left w-[768px]">
+      <ConnectButton />
+      <div className="flex flex-col gap-2">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Airdrop</h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">A simple tool to airdrop on Kaia chain</p>
+      </div>
+      <div className="flex flex-col gap-8">
         <AirdropKaiaCard />
         <AirdropErc20Card />
       </div>
--- components/airdrop-erc20-card.tsx
+++ components/airdrop-erc20-card.tsx
@@ -1,33 +1,47 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
+import Link from "next/link";
+import { ArrowRight, Loader2, Check } from "lucide-react";
 import { Input } from "@/components/ui/input";
-import {
-  type BaseError,
-  useReadContract,
-} from "wagmi";
-import { Address } from 'viem';
-import { useState } from 'react';
+import { type BaseError, useReadContract } from "wagmi";
+import { Address } from "viem";
+import { useState } from "react";
 
 export default function AirdropErc20Card() {
   const [contractAddress, setContractAddress] = useState("");
+  const { data: symbol, error, isPending } = useReadContract({
+    functionName: "symbol",
+    args: [contractAddress],
+  });
 
-  function handleContractAddressChange(event: React.ChangeEvent<HTMLInputElement>) {
+  function handleContractAddressChange(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
     setContractAddress(event.target.value);
   }
 
   return (
     <div className="flex flex-row justify-between items-center shadow-lg w-full p-8 border-2 border-secondary hover:border-primary">
-      <div className='flex flex-col gap-4'>
-        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">Airdrop $KAIA</h2>
+      <div className="flex flex-col gap-4">
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
+          Airdrop $KAIA
+        </h2>
         <p>Add contract address to check before proceeding</p>
-        <Input
-          type="text"
-          placeholder="0x..."
-          value={contractAddress}
-          onChange={handleContractAddressChange}
-        />
+        <div className="flex flex-row gap-4 items-center">
+          <Input
+            type="text"
+            placeholder="0x..."
+            value={contractAddress}
+            onChange={handleContractAddressChange}
+          />
+          {isPending && <Loader2 className="h-6 w-6 animate-spin text-yellow-500" />}
+          {symbol ? (
+            <p>
+              <Check className="mr-2 h-4 w-4 text-green-500" />
+              {symbol?.toString()}
+            </p>
+          ) : null}
+        </div>
       </div>
       <ArrowRight className="w-8 h-8" />
     </div>
