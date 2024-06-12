4bb8d25ae4e90b0f06930035c616e8aa4128e973
ZxStim <thang.victor.luong@gmail.com>
2024-06-12 23:36:15 +0700

    edit home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
 export default function Home() {
-
   return (
-    <div>
-      <h1>Airdrop tool</h1>
+    <div className="text-left w-[768px]">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Airdrop</h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">A simple tool to airdrop on Kaia chain</p>
     </div>
   );
 }
