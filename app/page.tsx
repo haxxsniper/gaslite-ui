ef3f512559d5e3dfff0e2be9a39bca6415eb3a86
ZxStim <thang.victor.luong@gmail.com>
2024-06-12 13:08:00 +0700

    made changes to airdrop klay ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { AirdropERC20 } from '@/components/airdrop-erc20';
-import { AirdropERC721 } from '@/components/airdrop-erc721';
-import { AirdropKlay } from '@/components/airdrop-klay';
 import Credits from '@/components/credits';
 import MobileWarning from '@/components/mobile-warning';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
@@ -11,9 +8,6 @@ export default function Home() {
     <main className="flex flex-col gap-8 items-center justify-center py-12 px-4 lg:p-36">
       <div className="hidden lg:flex lg:flex-col lg:gap-12 max-w-3xl">
         <ConnectButton />
-        <AirdropKlay />
-        <AirdropERC20 />
-        <AirdropERC721 />
         <Credits />
       </div>
       <MobileWarning />
